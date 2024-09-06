4a03fd54a69c46b223dadcc1e6a8e0b71d53692a
rmajjad <ramamajjad5@gmail.com>
2024-09-05 20:49:32 -0700

    travel app

--- src/client/__test__/RDays.test.js
+++ src/client/__test__/RDays.test.js
@@ -3,13 +3,13 @@ const { getRDays } = require("../scripts/RDays.js");
 
 
 const now = new Date();
-const travelDate = new Date("2024-09-15");
+const travelDate = new Date("2024-10-15");
 
 const daysLeft = travelDate.getTime() - now.getTime();
     
 const RDays = Math.ceil(daysLeft / (1000 * 3600 * 24));
 
-test('the remaining days from now', () => {
+test('the remaining days from now', () => { 
     expect(getRDays(travelDate)).toBe(RDays);
 });
 
